aa84eb328d1650c4b83639aa8cf01a2f4b32ba43
Ahron <Ahron@10.146.193.6>
2025-04-15 10:45:22 -0300

    Exemplo de matriz

---
+++ src/main/java/com/mycompany/matrizes/Matrizes.java
@@ -0,0 +1,23 @@
+package com.mycompany.matrizes;
+
+/**
+ *
+ * @author Ahron
+ */
+import javax.swing.*;
+ public class Matrizes {
+     public static void main(String args[ ])  {
+          double A[ ][ ] = new double[3][4];
+          int i, j;
+          
+          for(i=0; i<3; i++) {
+             for (j=0; j<4; j++){
+               A[i][j] = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da linha "+i+" e coluna "+j+": "));
+             }
+       }
+          
+          for(i=0; i<3; i++){
+         for (j=0; j<4; j++){
+            JOptionPane.showMessageDialog(null, "O valor contido na linha "+i+" e coluna "+j+" é " + A[i][j]);
+         } }
+  } }
